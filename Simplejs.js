b325a81ad65512aa912c23148170d34aa3d050f4
Rahul Shailesh Vaghela <50204902+DrRahulVaghelaMusic@users.noreply.github.com>
2019-05-09 00:01:19 +0530

    Add files via upload

---
+++ Simplejs.js
@@ -0,0 +1,10 @@
+document.getElementById('Acceptcookie').addEventListener('click',
+    function() {
+        document.querySelector('.CookiePolicypagepop').style.display = "none";
+        const date = new Date();
+        date.setTime(date.getTime() + 24 * 60 * 60 * 1000);
+        document.AcceptButtncooki.expires = '7Minutes';
+    });
+document.getElementById('cokkieclose').addEventListener('click', function() {
+    document.querySelector('.CookiePolicypagepop').style.display = "none";
+});
