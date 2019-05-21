6f873c56eb143f334e3eaa0363631d830878fa9d
Rahul Shailesh Vaghela <50204902+DrRahulVaghelaMusic@users.noreply.github.com>
2019-05-21 23:06:52 +0530

    Update Simplejs.js

--- Simplejs.js
+++ Simplejs.js
@@ -9,19 +9,3 @@ document.getElementById('cokkieclose').addEventListener('click', function() {
     document.querySelector('.CookiePolicypagepop').style.display = "none";
 });
 
-function openNav() {
-            document.getElementById("Hamburger").style.marginLeft = " 210px ";
-            document.getElementById("SitesliderNav").style.width = " 210px ";
-            document.body.style.backgroundColor = "rgba(0,0,0,0.3)";
-        }
-
-        function closeNav() {
-            document.getElementById("Hamburger").style.marginLeft = "0";
-            document.getElementById("SitesliderNav").style.width = " 0";
-            document.body.style.backgroundColor = "white";
-        }
-        window.addEventListener("load", function() {
-            const Loader = document.querySelector(".Loader");
-            Loader.className += " hidden";
-        });
-
